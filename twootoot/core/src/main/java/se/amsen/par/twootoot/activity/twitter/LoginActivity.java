36a11ad4d1ef8e75dacc4fd4c9bcea1a3ee2a9d7
paramsen <par.nils.amsen@gmail.com>
2015-11-08 01:19:08 +0100

    rewrote LoginActivity to use Java 8 lambda syntax

--- twootoot/core/src/main/java/se/amsen/par/twootoot/activity/twitter/LoginActivity.java
+++ twootoot/core/src/main/java/se/amsen/par/twootoot/activity/twitter/LoginActivity.java
@@ -2,7 +2,6 @@ package se.amsen.par.twootoot.activity.twitter;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import java.util.concurrent.TimeUnit;
 
@@ -10,10 +9,7 @@ import se.amsen.par.twootoot.R;
 import se.amsen.par.twootoot.activity.BaseActivity;
 import se.amsen.par.twootoot.behavior.NetworkExceptionBehavior;
 import se.amsen.par.twootoot.component.twitter.LoginComponent;
-import se.amsen.par.twootoot.model.twitter.OAuthConfig;
 import se.amsen.par.twootoot.source.twitter.OAuthSource;
-import se.amsen.par.twootoot.source.twitter.result.Result;
-import se.amsen.par.twootoot.util.functional.Callback;
 import se.amsen.par.twootoot.webcom.twitter.exceptions.MissingOAuthConfigException;
 
 public class LoginActivity extends BaseActivity {
@@ -31,38 +27,22 @@ public class LoginActivity extends BaseActivity {
 	}
 
 	private void authorize() {
-		hasConnection(new Callback<Boolean>() {
-			@Override
-			public void onComplete(Boolean result) {
-				if(result) {
-					new OAuthSource(LoginActivity.this).authorizeAsync(null, new Callback<Result<OAuthConfig>>() {
-						@Override
-						public void onComplete(Result<OAuthConfig> result) {
-							if (result.isSuccess()) {
-								startActivity(new Intent(LoginActivity.this, HomeActivity.class));
-								finish();
-							} else {
-								if (result.asFailure().get() instanceof MissingOAuthConfigException) {
-									login.onReady(null);
-								} else {
-									NetworkExceptionBehavior.showSnackbar(LoginActivity.this, new View.OnClickListener() {
-										@Override
-										public void onClick(View v) {
-											authorize();
-										}
-									});
-								}
-							}
+		hasConnection(result -> {
+			if(result) {
+				new OAuthSource(LoginActivity.this).authorizeAsync(null, result1 -> {
+					if (result1.isSuccess()) {
+						startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+						finish();
+					} else {
+						if (result1.asFailure().get() instanceof MissingOAuthConfigException) {
+							login.onReady(null);
+						} else {
+							NetworkExceptionBehavior.showSnackbar(LoginActivity.this, v -> authorize());
 						}
-					}, TimeUnit.SECONDS, 30);
-				} else {
-					NetworkExceptionBehavior.showSnackbar(LoginActivity.this, new View.OnClickListener() {
-						@Override
-						public void onClick(View v) {
-							authorize();
-						}
-					});
-				}
+					}
+				}, TimeUnit.SECONDS, 30);
+			} else {
+				NetworkExceptionBehavior.showSnackbar(LoginActivity.this, v -> authorize());
 			}
 		});
 	}
